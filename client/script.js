83d7c5af0ea25d2168f8436f8aa546eb8ba24e88
Kala Foods <guhan.delta@gmail.com>
2023-01-16 21:53:35 -0500

    Updated teh server URL on the UI

--- client/script.js
+++ client/script.js
@@ -88,7 +88,7 @@ const handleSubmit = async (e) =>{
   loader(messageDiv);
 
   // Fetch teh bot's response
- const response = await fetch('http://localhost:5000',{
+ const response = await fetch('https://codex-ydtd.onrender.com',{
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
